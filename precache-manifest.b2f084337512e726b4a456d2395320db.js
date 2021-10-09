018598931d8dcc3cc51c0edf8f07b9726469f008
Imon-Ovbude <imonhimio@gmail.com>
2021-10-09 16:13:59 -0700

    Updates

---
+++ precache-manifest.b2f084337512e726b4a456d2395320db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c018276c7c70c47b8d49",
+    "url": "/refugee-webiste/static/css/main.013c86fb.chunk.css"
+  },
+  {
+    "revision": "c018276c7c70c47b8d49",
+    "url": "/refugee-webiste/static/js/main.c018276c.chunk.js"
+  },
+  {
+    "revision": "4daa0e895f2a525cdc98",
+    "url": "/refugee-webiste/static/js/runtime~main.4daa0e89.js"
+  },
+  {
+    "revision": "df519a8cc2d86bebfff3",
+    "url": "/refugee-webiste/static/css/2.db86f7ab.chunk.css"
+  },
+  {
+    "revision": "df519a8cc2d86bebfff3",
+    "url": "/refugee-webiste/static/js/2.df519a8c.chunk.js"
+  },
+  {
+    "revision": "cbf90d710ab6b01bc52551e86a3dfeb1",
+    "url": "/refugee-webiste/index.html"
+  }
+];
